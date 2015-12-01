d3c45c6d72dbd670bb5d35a01c6364c2acc14e4c
araghavan <ashish.raghavan@halosys.com>
2015-12-01 20:16:33 +0530

    Initial commit for ShopEasy.

---
+++ app/src/main/java/com/shopping/shopeasy/fragment/SplashActivityFragment.java
@@ -0,0 +1,24 @@
+package com.shopping.shopeasy.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shopping.shopeasy.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SplashActivityFragment extends Fragment {
+
+    public SplashActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+}
